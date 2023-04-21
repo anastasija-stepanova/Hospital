089967f0699f66fd9c6fdf1d86348006aa11216a
Anastasiia Stepanova <nastyaaa19980@gmail.com>
2023-04-21 08:27:06 +0300

    Add create medical appointment route and controller

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/doctor', [DoctorController::class, 'create']);
 Route::post('/patient', [PatientController::class, 'create']);
+Route::post('/appointment', [MedicalAppointmentController::class, 'create']);
